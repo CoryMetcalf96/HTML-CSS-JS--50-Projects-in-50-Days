91c9dff78d60ac75b5180fcc3a8ca3dd26f09b6a
Cory Metcalf <corymetcalf96@gmail.com>
2022-10-03 12:16:29 -0400

    Finished 'Hidden Search' follow-along and added notes.:

---
+++ Hidden Search Widget/script.js
@@ -0,0 +1,10 @@
+// Set up variables for elements with the class search, button, and input.
+const search = document.querySelector('.search');
+const btn = document.querySelector('.btn');
+const input = document.querySelector('.input');
+
+// When the button is clickes, toggle the 'active' class alongside the search div and set the focus on your input element.
+btn.addEventListener('click', () => {
+    search.classList.toggle('active');
+    input.focus();
+})
