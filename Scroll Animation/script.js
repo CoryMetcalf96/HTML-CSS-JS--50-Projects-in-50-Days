1c3ee729a6acf3a0428396e8363402c7bbdd936a
Cory Metcalf <corymetcalf96@gmail.com>
2022-10-26 12:27:41 -0400

    Added notes and finished 'Scroll Animation' follow-along

--- Scroll Animation/script.js
+++ Scroll Animation/script.js
@@ -1,7 +1,10 @@
+// Create a node list for all elements with the class 'box'.
 const boxes = document.querySelectorAll('.box');
 
+// Create an event listener in the window that runs a 'checkBoxes' function when the user scrolls.
 window.addEventListener('scroll', checkBoxes)
 
+// Create a function that checks the box placements. Start by determining a 'triggerBottom' (where the boxes will scroll in). You can do this by dividing the inner height of the window by 5 and then multiplying by 4. Then, cycle through the node list. Establish the top of the box in to a variable for comparison against the trigger using box.getBoundingClientRect().top . Then, if the box top is less than the trigger, add the 'show' class to the box. If it's not, remove the 'show' class.
 function checkBoxes() {
     const triggerBottom = window.innerHeight / 5 * 4;
 
